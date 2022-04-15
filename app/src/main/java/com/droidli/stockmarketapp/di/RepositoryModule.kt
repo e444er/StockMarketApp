18d92c9c87deff0ef5462a7778dd9e722a2cdcdb
droid li <e444er@gmail.com>
2022-04-15 16:12:57 +0600

    screen

---
+++ app/src/main/java/com/droidli/stockmarketapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.droidli.stockmarketapp.di
+
+import com.droidli.stockmarketapp.data.csv.CSVParser
+import com.droidli.stockmarketapp.data.csv.CompanyListingParser
+import com.droidli.stockmarketapp.data.repository.StockRepositoryImpl
+import com.droidli.stockmarketapp.domain.model.CompanyListing
+import com.droidli.stockmarketapp.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
